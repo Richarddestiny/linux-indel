81b4cfd21d3fce0a1225748bdbdd0085acbbb25e
Valentin Zuellig <zuellig@indel.ch>
2017-07-07 15:52:56 +0200

    ! updated build script. creates tar archive which is better to copy to the i.mx6

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,3 +11,13 @@ make -j4 zImage LOADADDR=0x10008000
 make imx6d-ginhmi.dtb
 
 make modules
+
+
+# Copy all modules into a debug folder called dbg_lib.
+#The Yocto build automatically copies the modules into the rootfs
+
+make INSTALL_MOD_PATH=$(pwd)/dbg_lib modules_install
+
+tar -cf kernel-modules.tar -C dbg_lib .
+
+rm -rf dbg_lib
